4960ad7ad9a25a874aa99f3c5682905965254da6
John Chappelle <jrchap82@gmail.com>
2018-04-14 17:34:49 -0400

    chargeForm style changes and profile.js

--- components/chargeForm.js
+++ components/chargeForm.js
@@ -1,25 +1,28 @@
-import React from "react"
-import t from "tcomb-form-native"
-import { StyleSheet, Text, View, Button } from "react-native"
+import React from "react";
+import t from "tcomb-form-native";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
 
-var Form = t.form.Form
+var Form = t.form.Form;
 
 const chargeForm = t.struct({
   charge: t.String,
   chargeAmt: t.Number
-})
+});
 
 export default class ChargeForm extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Form type={chargeForm}/>
-        <Button
-          title="Submit"
-          color="#85bb65"
-        />
+        <Form type={chargeForm} />
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.onPress}
+          underlayColor="#99d9f4"
+        >
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableHighlight>
       </View>
-    )
+    );
   }
 }
 
@@ -29,5 +32,22 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+
+  buttonText: {
+    fontSize: 18,
+    color: "white",
+    alignSelf: "center"
+  },
+
+  button: {
+    height: 36,
+    backgroundColor: "#48BBEC",
+    borderColor: "#48BBEC",
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: "stretch",
+    justifyContent: "center"
   }
-})
+});
