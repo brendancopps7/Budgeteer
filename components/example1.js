e67a1a50165abe653bedfb550b7a1325f99fd64f
Nathaniel <natedrexdrag@gmail.com>
2018-04-14 14:40:44 -0400

    "Button test"

--- components/example1.js
+++ components/example1.js
@@ -1,13 +1,56 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Example1 extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>WOW THIS WORKED</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Button
+          title='Super Cool Button'
+        />
+        <Button
+          icon=
+          {
+            <Icon
+              name='arrow-right'
+              size={15}
+              color='white'
+            />
+          }
+          title='BUTTON WITH ICON'
+        />
+
+        <Button
+          icon=
+          {
+            <Icon
+              name='arrow-right'
+              size={15}
+              color='white'
+            />
+          }
+          iconRight
+          title='BUTTON WITH RIGHT ICON'
+        />
+
+        <Button
+          title="LOADING BUTTON"
+          loading
+          loadingProps={{ size: "large", color: "rgba(111, 202, 186, 1)" }}
+          titleStyle={{ fontWeight: "700" }}
+          buttonStyle=
+          {{
+            backgroundColor: "rgba(92, 99,216, 1)",
+            width: 300,
+            height: 45,
+            borderColor: "transparent",
+            borderWidth: 0,
+            borderRadius: 5
+          }}
+          containerStyle={{ marginTop: 20 }}
+        />
       </View>
     )
   }
