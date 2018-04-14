1cccc459979eea37bfa175f1f19a4effa2e07304
Nathaniel <natedrexdrag@gmail.com>
2018-04-14 15:27:39 -0400

    "button testing 0.1"

--- components/example1.js
+++ components/example1.js
@@ -1,56 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { StyleSheet, Text, View } from 'react-native'
 
 export default class Example1 extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>WOW THIS WORKED</Text>
+        <Text>Header</Text>
         <Button
-          title='Super Cool Button'
-        />
-        <Button
-          icon=
-          {
-            <Icon
-              name='arrow-right'
-              size={15}
-              color='white'
-            />
-          }
-          title='BUTTON WITH ICON'
-        />
-
-        <Button
-          icon=
-          {
-            <Icon
-              name='arrow-right'
-              size={15}
-              color='white'
-            />
-          }
-          iconRight
-          title='BUTTON WITH RIGHT ICON'
-        />
-
-        <Button
-          title="LOADING BUTTON"
-          loading
-          loadingProps={{ size: "large", color: "rgba(111, 202, 186, 1)" }}
-          titleStyle={{ fontWeight: "700" }}
-          buttonStyle=
-          {{
-            backgroundColor: "rgba(92, 99,216, 1)",
-            width: 300,
-            height: 45,
-            borderColor: "transparent",
-            borderWidth: 0,
-            borderRadius: 5
+          onPress={() => {
+            Alert.alert('You tapped the button!');
           }}
-          containerStyle={{ marginTop: 20 }}
+          title="Press Me"
         />
+        <Text>Footer</Text>
       </View>
     )
   }
@@ -59,8 +21,13 @@ export default class Example1 extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#9533FF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonStyle: 
+  {
+
+
+  }
 })
