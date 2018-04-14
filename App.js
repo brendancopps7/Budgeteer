f6eac9b1f3b84b45cdda7b75677eebd9aee40f2a
Morgan Wallace <mlw343@drexel.edu>
2018-04-14 15:00:01 -0400

    Some menu stuff for all pages

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { View, Text,Button } from 'react-native'
 import Example1 from './components/example1'
+import Menu from './components/menu'
 import config from './oauth2-config'
+import SideMenu from 'react-native-side-menu'
 
 export default class App extends React.Component {
   constructor(){
@@ -8,9 +11,14 @@ export default class App extends React.Component {
     let scope = 'https://www.googleapis.com/auth/sqlservice.admin'
   }
 
+
+
   render() {
+    let menu = <Menu/>
     return (
-      <Example1/>
+      <SideMenu menu={menu}>
+        <Example1/>
+      </SideMenu>
     )
   }
 }
