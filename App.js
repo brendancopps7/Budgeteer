db5df20952ccf699ca96189e1203e4d6ba447df2
Morgan Wallace <mlw343@drexel.edu>
2018-04-14 14:36:04 -0400

    no message

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import Example from './components/example'
+import config from './oauth2-config'
 
 export default class App extends React.Component {
+  constructor(){
+    super()
+    let scope = 'https://www.googleapis.com/auth/sqlservice.admin'
+  }
+
   render() {
     return (
       <Example/>
