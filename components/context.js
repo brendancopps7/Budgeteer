febbffc9fc721fe9d24a0c6020d96606eaf41c0b
Nathaniel <natedrexdrag@gmail.com>
2018-04-14 16:46:26 -0400

    example1.js --> homepage.js

--- components/context.js
+++ components/context.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Example1 from './example1'
+import Homepage from './homepage'
 import {inject, observer} from 'mobx-react'
 
 @inject('page') @observer
 export default class Context extends React.Component {
   render() {
     return (
-      <Example1/>
+      <Homepage/>
     )
   }
 }
